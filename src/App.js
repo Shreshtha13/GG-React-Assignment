a2d9b97ef37c024b64f1cbcf4a2eb3dc09c095d5
Shreshtha13 <shreshtha_m@cs.iitr.ac.in>
2021-09-29 09:36:49 +0530

    add hover effect

--- src/App.js
+++ src/App.js
@@ -44,7 +44,7 @@ const App = () => {
           Cast: response.data.Actors,
           Year: response.data.Year,
           imdbRating: response.data.imdbRating,
-          Poster : response.data.Poster
+          Poster: response.data.Poster
         })
       }
     }
@@ -64,7 +64,9 @@ const App = () => {
       movieList.map(m => {
         return (
           <div key={m.imdbID + m.Year} className="card">
-            <p key={m.Year} onClick={displayInfo} style={{ cursor: 'pointer' }}>{m.Title} </p>
+            <div className='hoverCard'>
+              <p key={m.Year} onClick={displayInfo} style={{ cursor: 'pointer' }}>{m.Title} </p>
+            </div>
             <img key={m.imdbID} src={m.Poster} alt="Movie Poster" />
           </div>
         )
@@ -72,29 +74,29 @@ const App = () => {
     )
   }
 
-  if(movieDetails.Title!==undefined){
+  if (movieDetails.Title !== undefined) {
 
   }
 
-  const closeDisplay = ()=>{
+  const closeDisplay = () => {
     setMovieDetails({})
   }
-  
+
   return (
     <div>
       <form>
         <input className='searchBox' type='text' onChange={handleSearchChange} name="search" />
       </form>
-      <div className="displayCard" style={{visibility:movieDetails.Title===undefined?'hidden' : 'visible'}}>
+      <div className="displayCard" style={{ visibility: movieDetails.Title === undefined ? 'hidden' : 'visible' }}>
         <button onClick={closeDisplay}>Close</button>
-        <img src={movieDetails.Poster} alt='movie poster'/>
+        <img src={movieDetails.Poster} alt='movie poster' />
         <p>Title : {movieDetails.Title}</p>
         <p>Year : {movieDetails.Year}</p>
         <p>Cast : {movieDetails.Cast}</p>
         <p>Plot : {movieDetails.Plot}</p>
         <p>imdb Rating : {movieDetails.imdbRating}</p>
       </div>
-      <div className="movieCards" style={{filter : movieDetails.Title!==undefined ? 'blur(10px)' : 'blur(0px)' }}>
+      <div className="movieCards" style={{ filter: movieDetails.Title !== undefined ? 'blur(10px)' : 'blur(0px)' }}>
         <MoviesList></MoviesList>
       </div>
     </div>
